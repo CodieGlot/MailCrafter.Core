d901123596d48cf6d1e560393782a733676aae21
Codie <ngnhathuy1224@gmail.com>
2025-01-13 20:35:04 +0700

    update 13/1

--- Domain/AppUser/EmailAccount.cs
+++ Domain/AppUser/EmailAccount.cs
@@ -3,5 +3,6 @@ public class EmailAccount
 {
     public EmailAccountStatus Status { get; set; }
     public string Email { get; set; }
+    public string Alias { get; set; }
     public string AppPassword { get; set; }
 }
